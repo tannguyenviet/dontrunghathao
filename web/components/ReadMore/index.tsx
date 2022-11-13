534a4bb84342be22c77b5a0b3ec10feaa5a04d8d
TanNguyenViet <nguyenviettantan@gmail.com>
2022-11-13 16:19:32 +0700

    add feet-1

--- web/components/ReadMore/index.tsx
+++ web/components/ReadMore/index.tsx
@@ -2,10 +2,10 @@ import NextIcon from 'assets/svg/Next';
 import Image from 'next/image';
 import React from 'react';
 
-const ReadMore = ({link}:any) => {
+const ReadMore = ({ link }: any) => {
   return (
-    <a href='#' target="_blank" rel="noopener noreferrer"className="f-start red opacity-8">
-      <span className='mr-8 red bold'>Read more</span>
+    <a href={link} target="_blank" rel="noopener noreferrer" className="f-start red opacity-8">
+      <span className="mr-8 red bold">Read more</span>
       <NextIcon color="#8a1722" />
     </a>
   );
